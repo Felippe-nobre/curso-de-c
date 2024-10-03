8622c6927248fff4be55bc041172e685650ade94
Felippe Nobre <113641116+Felippe-nobre@users.noreply.github.com>
2024-10-03 01:37:48 -0300

    .

---
+++ atividade-em-c/atividade6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int dia;
+
+    printf("Digite um número (1-7) para o dia da semana: ");
+    scanf("%d", &dia);
+
+    switch(dia) {
+        case 1:
+            printf("Domingo\n");
+            break;
+        case 2:
+            printf("Segunda-feira\n");
+            break;
+        case 3:
+            printf("Terça-feira\n");
+            break;
+        case 4:
+            printf("Quarta-feira\n");
+            break;
+        case 5:
+            printf("Quinta-feira\n");
+            break;
+        case 6:
+            printf("Sexta-feira\n");
+            break;
+        case 7:
+            printf("Sábado\n");
+            break;
+        default:
+            printf("Número inválido! Digite um número de 1 a 7.\n");
+    }
+
+    return 0;
+}
