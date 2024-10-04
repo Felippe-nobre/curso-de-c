2ae7970d3a2109391b5ff99f8d33912e9daebd3b
Felippe Nobre <113641116+Felippe-nobre@users.noreply.github.com>
2024-10-03 23:24:09 -0300

    novo projeto

---
+++ atividade/atividade.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct 
+{
+   char Nome[50];
+   float Preco;
+   int Quantidade;
+}Produto;
+
+typedef struct 
+{
+    int Id;
+    Produto Produtos[99];
+    float Total;
+}Pedido;
+
+
+int main(){
+    int status = 1, id = 0, c;
+    Pedido Pedidos[99];
+
+    while (status){
+    printf("\e[1;1H\e[2J");
+    
+        printf("\nescolha uma opecao: ");
+        int opecoes;
+        scanf("%d", &opecoes);
+
+        switch (opecoes)
+        {
+        case 1:
+        printf("\e[1;1H\e[2J");
+            Pedidos[id]. Id = id;
+            printf("\n pedido Id: %d \n", id + 1);
+            float total = 0;
+
+            for (int i = 0; i < 99; i++)
+            {
+                 while((c = getchar()) != '\n' && c != EOF);
+
+
+                printf("\nDigite o nome do produto: ");
+                char nome[50];
+                fgets(nome, sizeof(nome), stdin);
+
+                printf("\nDigite o preco do produto: ");
+                float preco;
+                scanf("%f", &preco);
+
+                printf("\nDigite o quantidade do produto: ");
+                int quantidade;
+                scanf("%d", &quantidade);
+                Produto produto = {.Preco = preco, .Quantidade = quantidade};
+                strcpy(produto.Nome, nome);
+                Pedidos[id].Produtos[i] = produto;
+
+                total += preco * quantidade;
+
+                printf("Voce deseja adicionar mais produtos? [y/n]");
+                char novoProduto;
+                scanf(" %c", &novoProduto);
+                if (novoProduto != 'y')
+                {
+                    break;
+                }
+                
+                
+            }
+            Pedidos[id].Total = total;
+            id++;
+            break;
+
+        case 2:
+        printf("\e[1;1H\e[2J");
+            for (int i = 0; i < id; i++)
+            {
+                printf("Pedidos Id: %d", i + 1);
+               for (int j = 0; j < 99; j++)
+               {
+                printf("Produto #%d", j + 1);
+
+                printf("\nProdutos nome: %s", Pedidos[i].Produtos[j].Nome);
+                printf("\nProdutos preco: %.f",Pedidos[i].Produtos[j].Preco); 
+                printf("\nProdutos quantidade: %d",Pedidos[i].Produtos[j].Quantidade);  
+
+                if (Pedidos[i].Produtos[j + 1].Quantidade == 0)
+                {
+                    break;
+                }
+                
+               }
+               printf("\nTotal: %.f", Pedidos[i].Total);
+               
+            }
+            printf("\nPressione uma tecla para continuar...");
+             while((c = getchar()) != '\n' && c != EOF);
+            getchar();
+            
+            break;
+
+        case 3:
+
+        status = 0;
+
+            break;    
+
+        
+        
+        }
+    }
+    
+
+return 0;
+}
