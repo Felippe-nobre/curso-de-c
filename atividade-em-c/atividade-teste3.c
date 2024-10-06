feab477a16286bf519d24925b99ce9c5242cb88c
Felippe Nobre <113641116+Felippe-nobre@users.noreply.github.com>
2024-10-05 21:40:45 -0300

    .

--- atividade-em-c/atividade-teste3.c
+++ atividade-em-c/atividade-teste3.c
@@ -7,12 +7,7 @@ int main(){
 
     printf("Digite um numero de 1 a 12: ");
     scanf("%d", &mes);
- if (mes <= 0 || mes >=13)
-    {
-        printf("Numeor errado .... porfavor digite numero de 1 a 12\n");
-        printf("Tente novamente:");
-        scanf("%d",&mes);
-    } 
+
     switch (mes)
     {
     case 1:
@@ -64,7 +59,7 @@ int main(){
 
     
     default:
-   
+    
     
         break;
     }
